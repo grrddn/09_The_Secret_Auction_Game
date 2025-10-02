32afaaad7e2c07d553fde2a472829068a4cef5c3
Daniel Ortega <grrddn@hotmail.com>
2025-10-02 15:09:49 -0500

    Se crearon dos archivos de python, main.py contiene el print y el llamado de la función. El segundo archivo, auction_game contiene la función y el programa como tal.

---
+++ auction_game.py
@@ -0,0 +1,23 @@
+def auction_game():
+    auction = {}
+    bidders = True
+
+    while bidders == True:
+      name = input("Type in your name please: \n")
+      bid = int(input("Type in your bid please: \n"))
+      auction[name] = bid
+      bidders = input("Are there anymore bidders? Type 'yes' or 'no': \n")
+
+      if bidders == "no":
+        bidders = False
+      else:
+        bidders = True
+        print("\n" * 10)
+
+    highest_bid = 0
+    for i in auction:
+      if auction[i] > highest_bid:
+        highest_bid = auction[i]
+        winner = i
+
+    print(f"The winner is {winner} with a bid of {highest_bid}")
---
+++ main.py
@@ -0,0 +1,11 @@
+import auction_game
+print('''
+
+      Welcome to the Secret Auction Game
+      🔨  💰  🏷️
+
+
+''')
+
+
+auction_game.auction_game()
